ef6c7ce50a0b95c5384eafcd73ab31f96ce88bc2
PCJOSE <josedanielrodriguezcastro@gmail.com>
2021-02-03 21:14:07 +0100

    Resta i mult

--- src/BigNumber.java
+++ src/BigNumber.java
@@ -91,12 +91,186 @@ class BigNumber {
 
     // Resta
     BigNumber sub(BigNumber other) {
-        return null;
+        BigNumber sub = new BigNumber("");
+        int carry = 0;
+
+        if (this.number.charAt(0) == '0') {
+            this.RemoveZeros(this);
+        }
+
+        if (other.number.charAt(0) == '0') {
+            other.RemoveZeros(other);
+        }
+
+        if (this.number.charAt(0) == '0' && other.number.charAt(0) == '0') {
+            sub.number = "0";
+            return sub;
+        }
+
+        if (this.number.length() == other.number.length()) {
+            for (int i = this.number.length()-1; i >= 0 ; i--) {
+                char char1 = this.number.charAt(i);
+                char char2 = (char) (other.number.charAt(i) + carry);
+                char subChar = 0;
+
+                if (carry == 1) carry--;
+
+                if (char2 > char1) {
+                    subChar = (char) (58 - char2 + char1);
+                    carry++;
+                }
+
+                if (char1 > char2) {
+                    subChar = (char) (char1 - char2 + 48);
+                }
+
+                if (char1 == char2) {
+                    subChar = 48;
+                }
+
+                sub.number += subChar;
+            }
+        }
+
+        if (this.number.length() > other.number.length()) {
+            other.AddZeros(this);
+            for (int i = this.number.length()-1; i >= 0 ; i--) {
+                char char1 = this.number.charAt(i);
+                char char2 = (char) (other.number.charAt(i) - carry);
+                char subChar = 0;
+                if (carry == -1) carry++;
+                if (char2 > char1) {
+                    subChar = (char) (58 - char2 + char1);
+                    carry--;
+                }
+
+                if (char1 > char2) {
+                    subChar = (char) (char1 - char2 + 48);
+                }
+
+                if (char1 == char2) {
+                    subChar = 48;
+                }
+
+
+                sub.number += subChar;
+
+
+            }
+        }
+
+        if (this.number.length() < other.number.length()) {
+            this.AddZeros(other);
+            for (int i = this.number.length()-1; i >= 0 ; i--) {
+                char char1 = this.number.charAt(i);
+                char char2 = (char) (other.number.charAt(i) - carry);
+                char subChar = 0;
+                if (carry == -1) carry++;
+                if (char2 > char1) {
+                    subChar = (char) (58 - char2 + char1);
+                    carry--;
+                }
+
+                if (char1 > char2) {
+                    subChar = (char) (char1 - char2 + 48);
+                }
+
+                if (char1 == char2) {
+                    subChar = 48;
+                }
+
+
+                sub.number += subChar;
+
+
+            }
+        }
+
+        sub.InvertString(sub);
+        sub.RemoveZeros(sub);
+        return sub;
+
     }
 
     // Multiplica
     BigNumber mult(BigNumber other) {
+        int carry = 0;
+        String results = "";
+
+        if (this.number.charAt(0) == '0') {
+            this.RemoveZeros(this);
+        }
+
+        if (other.number.charAt(0) == '0') {
+            other.RemoveZeros(other);
+        }
+
+        String[] addResults = new String[other.number.length()];
+
+        for (int i = other.number.length() - 1; i >= 0 ; i--) {
+            char char1 = (char) (other.number.charAt(i) - 48);
+            if (i < other.number.length() - 1) {
+                for (int j = 0; j < other.number.length()-1-i ; j++) {
+                    results += 0;
+                }
+            }
+
+            for (int j = this.number.length() - 1; j >= 0; j--) {
+                char char2 = (char) (this.number.charAt(j) - 48);
+
+                char adds = (char) (char2 * char1 + carry);
+                carry = 0;
+
+                while (adds >= 10) {
+                    adds -= 10;
+                    carry++;
+                }
+
+                results += (int)adds;
+            }
+            if (carry != 0) results += carry;
+
+            addResults[i] = results;
+            results = "";
+            carry = 0;
+        }
+
+        for (int i = 0; i < addResults.length; i++) {
+            String invert = "";
+            for (int j = addResults[i].length() -1; j >= 0; j--) {
+                invert += addResults[i].charAt(j);
+            }
+            addResults[i] = invert;
+        }
+
+        if (addResults.length == 1) {
+            BigNumber b1 = new BigNumber(addResults[0]);
+            b1.RemoveZeros(b1);
+            return b1;
+        }
+
+        if (addResults.length == 2) {
+            BigNumber b1 = new BigNumber(addResults[0]);
+            BigNumber b2 = new BigNumber(addResults[1]);
+            BigNumber bResult = new BigNumber(b1.add(b2).number);
+            bResult.RemoveZeros(b2);
+            return bResult;
+        }
+
+        if (addResults.length > 2) {
+            BigNumber b1 = new BigNumber(addResults[0]);
+            BigNumber b2 = new BigNumber(addResults[1]);
+            BigNumber addBigNumber = new BigNumber(b1.add(b2).number);
+
+            for (int i = 2; i < addResults.length; i++) {
+                addBigNumber.number = addBigNumber.add(new BigNumber(addResults[i])).number;
+            }
+
+            addBigNumber.RemoveZeros(addBigNumber);
+            return addBigNumber;
+        }
         return null;
+
     }
 
     // Divideix
